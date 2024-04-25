2fc4754646b9e7fb73b0626524374ca09c979885
AntonBabychP1T <antonbabi13@gmail.com>
2024-04-25 17:13:58 +0300

    [TEST] Create integral test for UserController

---
+++ src/test/resources/db/users/create-two-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, birth_date, email, address, phone_number, deleted)
+VALUES
+    ('John', 'Doe', '1990-05-15', 'john.doe@example.com', '123 Main St, City', '123-456-7890', false),
+    ('Jane', 'Smith', '1985-10-20', 'jane.smith@example.com', '456 Elm St, Town', '987-654-3210', false);
